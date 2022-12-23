a2d9ebbb228535af41d170e8e014e2dbcce8a9b3
Rizqi <103945666+Masdarul@users.noreply.github.com>
2022-12-23 10:56:46 +0700

    new

---
+++ masa percobaan/public/script.js
@@ -0,0 +1,37 @@
+// ambil element
+const getUsername = document.querySelector("#usename");
+const getPassword = document.querySelector("#password");
+const getForm = document.querySelector("form");
+
+
+// cek value
+// alert(getUsername.value)
+
+// buat user dan pass
+const USERS = [
+    {
+        name: "rizqi@yahoo.com",
+        password: "000",
+    },
+];
+// console.log(USERS);
+
+// event
+getForm.addEventListener("submit",(event) =>{
+    // agar saat user klik login tidak refresh
+    event.preventDefault();
+
+// alert(getUsername.value);
+// alert(getPassword.value);
+
+// lalukan perulangan untuk ambil data
+    USERS.forEach((user) => {
+        //percabangan
+        if(getUsername.value == user.Username && getPassword.value == user.Password) {
+            alert("oke berhasil login");
+        } else {
+            alert("Invalid Credentials");
+        }
+    });
+});
+
